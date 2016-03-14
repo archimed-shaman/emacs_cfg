dc189db9aad9ddd535973fecb870069308a341cc
a_morozov <a.morozov@semrush.com>
2016-03-14 15:01:05 +0300

    scala

--- .emacs.d/plugins/scala.el
+++ .emacs.d/plugins/scala.el
@@ -1,6 +1,42 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; scala
 
+;; 1) you have to install sbt by hand
+;; 2) make something like this:
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; // ensime-sbt is needed for the integration tests
+;; addSbtPlugin("org.ensime" % "ensime-sbt" % "0.4.0")
+;; 
+;; // BUG https://github.com/sbt/sbt-header/issues/31
+;; //addSbtPlugin("de.heikoseeberger" % "sbt-header" % "1.5.0")
+;; 
+;; // not working on Windows https://github.com/sbt/sbt/issues/1952
+;; //addMavenResolverPlugin
+;; 
+;; addSbtPlugin("org.scalariform" % "sbt-scalariform" % "1.5.1")
+;; 
+;; // waiting for 1.3.6 https://github.com/scoverage/sbt-scoverage/issues/153
+;; // addSbtPlugin("org.scoverage" %% "sbt-scoverage" % "1.3.5")
+;; // sbt-coveralls needs a new release
+;; // https://github.com/scoverage/sbt-coveralls/issues/52
+;; //addSbtPlugin("org.scoverage" %% "sbt-coveralls" % "1.0.1")
+;; 
+;; addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "0.14.1")
+;; 
+;; scalacOptions in Compile ++= Seq("-feature", "-deprecation")
+;; 
+;; // sbt, STFU...
+;; ivyLoggingLevel := UpdateLogging.Quiet
+;; 
+;; addSbtPlugin("com.eed3si9n" % "sbt-buildinfo" % "0.5.0")
+;; 
+;; resolvers += Resolver.sonatypeRepo("snapshots")
+;; 
+;; addSbtPlugin("org.ensime" % "ensime-sbt" % "0.1.5-SNAPSHOT")
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
 
 ;; highlight FIXME, TODO, etc
 (req_package 'fic-mode)
@@ -9,3 +45,5 @@
 
 (add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
 (add-hook 'scala-mode-hook '(lambda () (fic-mode 1)))
+
+(provide 'scala-loader)
