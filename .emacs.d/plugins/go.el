91d691702533ca1a23dba0a69409368fa0fd29ba
archimed <archimed.shaman@gmail.com>
2019-07-14 01:11:16 +0300

    go snippets

--- .emacs.d/plugins/go.el
+++ .emacs.d/plugins/go.el
@@ -5,15 +5,19 @@
 (req_package 'fic-mode)
 (req_package 'company)
 (req_package 'flycheck)
-(req_package 'yasnippet)
 (req_package 'multi-compile)
 (req_package 'go-eldoc)
 (req_package 'company-go)
+(req_package 'yasnippet)
 
+(add-to-list 'yas-snippet-dirs "/home/archimed/dev/emacs_cfg/.emacs.d/yasnippet-golang/")
+(yas-reload-all)
+(set 'yas-global-mode 1)
 
 ;; do:
 ;; go get -u github.com/mdempsky/gocode
 
+(setenv "GOPATH" (concat (getenv "HOME") "/go"))
 (setq exec-path (append exec-path '("~/go/bin")))
 
 (use-package go-mode
@@ -34,6 +38,8 @@
                             (set (make-local-variable 'company-backends) '(company-go))
                             (company-mode)))
 (add-hook 'go-mode-hook 'yas-minor-mode)
+
+
 (add-hook 'go-mode-hook 'flycheck-mode)
 (setq multi-compile-alist '(
     (go-mode . (
